fb70e5480edbaf153e082d12debd6dcdada7e72d
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-06-11 22:48:13 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'New text content';
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = "No number!"
+    }
+})
