e7bc2e8555a84000677b94089d43b3f12dc647fe
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-06-16 23:17:09 -0300

    finish

--- script.js
+++ script.js
@@ -7,37 +7,57 @@ console.log(secretNumber);
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess);
 
-    if(!guess) {
-        document.querySelector('.message').textContent = "Só pode números entre 1 e 20"
+        if(!guess) {
+            displayMessage('Só pode números entre 1 e 20')
 
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = "🎉 Acertou!"
-        document.querySelector('.number').textContent = secretNumber;
+            //document.querySelector('.message').textContent = "Só pode números entre 1 e 20"
 
-        document.querySelector('body').style.backgroundColor = "#60b347";
 
-        document.querySelector('.number').style.width  = '30rem'
+        } else if (guess === secretNumber) {
+                //document.querySelector('.message').textContent = "🎉 Acertou!"
+                displayMessage("🎉 Acertou!")
+                document.querySelector('.number').textContent = secretNumber;
 
-        if (score > highscore) {
-            highscore = score
-            document.querySelector('.highscore').textContent = highscore
-        }
+                document.querySelector('body').style.backgroundColor = "#60b347";
 
-    } else if (guess > secretNumber) {
+            document.querySelector('.number').style.width  = '30rem'
+
+            if (score > highscore) {
+                highscore = score
+                document.querySelector('.highscore').textContent = highscore
+            }
+
+
+        }  else if (guess !== secretNumber) {
+            if (score > 1) {
+                displayMessage(guess > secretNumber ? 'Está acima!' : 'Está abaixo')
+
+                //document.querySelector('.message').textContent = guess > secretNumber ? 'Está acima!' : 'Está abaixo'
+        
+                score--
+                document.querySelector('.score').textContent = score;
+            } else {
+                displayMessage('Você perdeu!')
+                //document.querySelector('.message').textContent = "Você perdeu!"
+                document.querySelector('.score').textContent = score - 1
+
+            }
+        }})
+
+    /*
+    else if (guess > secretNumber) {
         if (score > 1) {
 
-            document.querySelector('.message').textContent = "Está acima!"
-    
-            score--
-            document.querySelector('.score').textContent = score
 
         } else {
-            document.querySelector('.message').textContent = "Você perdeu!"
-            document.querySelector('.score').textContent = score - 1
         }
 
 
@@ -55,7 +75,7 @@ document.querySelector('.check').addEventListener('click', function() {
         }
     }
 })
-
+*/
 document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     secretNumber = Math.trunc(Math.random()*20)+1;
@@ -64,9 +84,11 @@ document.querySelector('.again').addEventListener('click', function() {
     document.querySelector('.number').style.width  = '15rem'
     document.querySelector('.number').textContent  = '?'
 
-    document.querySelector('.message').textContent = 'Começando a calcular...'
+    //document.querySelector('.message').textContent = 'Começando a calcular...'
+    displayMessage('Comceçando a calcular...')
+
     document.querySelector('.score').textContent = score
 
     document.querySelector('.guess').value = ''
     
-})
+}) 
